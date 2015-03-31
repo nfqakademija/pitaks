94b29fa64eb01f2b4f325270c9c6ecadc1c6ba8c
Mykolas Liaskauskas <mykolas.liaskauskas@gmail.com>
2015-03-31 18:36:06 +0300

    Modified views, controllers, routing to make static index page. Added Bootstrap

--- src/pitaks/KickerBundle/Controller/ProfileController.php
+++ src/pitaks/KickerBundle/Controller/ProfileController.php
@@ -6,16 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProfileController extends Controller
 {
-    public function indexAction()
+    public function indexAction($name)
     {
-        return $this->render('pitaksKickerBundle:Profile:index.html.twig', array(
-                // ...
-            ));    }
+        return $this->render('pitaksKickerBundle:Profile:index.html.twig', array('name' => $name));
+    }
 
     public function editAction()
     {
-        return $this->render('pitaksKickerBundle:Profile:edit.html.twig', array(
-                // ...
-            ));    }
+        return $this->render('pitaksKickerBundle:Profile:edit.html.twig');
+    }
 
 }
