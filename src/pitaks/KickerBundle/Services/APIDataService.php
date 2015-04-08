7da745913a1fcd2e33a40cb67e8f6b9b93a42e18
Aurimas Valancius <aurimas.valancius@gmail.com>
2015-04-08 18:49:19 +0300

    update

--- src/pitaks/KickerBundle/Services/APIDataService.php
+++ src/pitaks/KickerBundle/Services/APIDataService.php
@@ -12,9 +12,13 @@
  use GuzzleHttp\Client;
  use pitaks\KickerBundle\Entity\Tables;
  use pitaks\KickerBundle\Entity\EventTable;
+ use pitaks\KickerBundle\Event\ApiEvents;
+ use Symfony\Component\Config\Definition\Exception\Exception;
  use Symfony\Component\DependencyInjection\ContainerAware;
+ use pitaks\KickerBundle\EventListener\ApiEventListener;
+ use Symfony\Component\EventDispatcher\EventDispatcher;
 
-class APIDataService extends ContainerAware {
+ class APIDataService extends ContainerAware {
 
     protected $em;
 
@@ -39,37 +43,75 @@ class APIDataService extends ContainerAware {
         $this->em = $em;
     }
 
+     /**
+      * @param Table $table
+      * @return mixed
+      */
+        public function getLastActiveTableApiJson($table)
+        {
+            return $this->getJsonFromTableApi($table,1);
+        }
 
+     /**
+      * @param Tables $table
+      * @param $rows
+      * @param null $fromID
+      * @return mixed
+      */
+    public function getJsonFromTableApi($table, $rows, $fromID = null)
+    {
+        $client = new Client();
+        $results= null;
+        try {
+            if (!$fromID) {
+                $results = $client->
+                get($table->getApiUrl() . 'rows=' . $rows, ['auth' => [$table->getUserName(), $table->getPassword()]]);
+            } else {
+                $results = $client->
+                get($table->getApiUrl() . 'rows=' . $rows . '&from-id=' . $fromID, ['auth' => [$table->getUserName(), $table->getPassword()]]);
+            }
+        }
+        catch(Exception $e){
+
+        }
+        return $results->json();
+    }
     /**
      * @param $taleID
      */
     public function  getData($taleID)
-{
+    {
        /** need to check if id exits */
      if($this->getEm()->getRepository('pitaksKickerBundle:Tables')->checkTableId($taleID)) {
          //we will execute code where:
-         set_time_limit (100000000000000000000000000);
+         set_time_limit (0);
          $table =$this->getEm()->getRepository('pitaksKickerBundle:Tables')->findByID($taleID);
-
-         $client = new Client();
-         $fromID=$this->getEm()->getRepository('pitaksKickerBundle:EventTable')->getLastEvent();
-         $rows=100;
-
-         $results = $client->get($table->getApiUrl().'rows=1', ['auth' =>  [$table->getUserName(),  $table->getPassword()]]);
-         $data = $results->json();
+         $fromID=$this->getEm()->getRepository('pitaksKickerBundle:EventTable')->getLastEvent($taleID);
+         $data = $this->getLastActiveTableApiJson($table);
+         $dispatcher = new EventDispatcher();
+         $apiEvent = new ApiEvents();
+        /* if(!$results){
+            $dispatcher->dispatch($apiEvent::API_FAILED_EVENT, $apiEvent);
+         }
+         $dispatcher->dispatch($apiEvent::API_SUCCESS_EVENT, $apiEvent);
+*/
          $lastID= $data['records'][0]['id'];
-
          while($fromID<$lastID)
          {
              //set API address, later we will take it from tables class
-             $res = $client->get($table->getApiUrl().'rows='.$rows.'&from-id='.$fromID, ['auth' =>  [$table->getUserName(),  $table->getPassword()]]);
-             $data = $res->json();
+            $data =  $data = $this->getJsonFromTableApi($table, 100, $fromID);
              $records = $data['records'];
              //we go
              foreach ($records as $record) {
                  //create new event record
-                 $event = new EventTable($record['id'],$record["timeSec"],$record["usec"],$record["type"],$record["data"],$table);
-                 //no brand found. So, persist the new one:
+                 $event = new EventTable();
+                 $event->setData($record["data"])
+                     ->setId($record['id'])
+                     ->setTableId($table)
+                     ->setTimeSec($record["timeSec"])
+                     ->setType($record["type"])
+                     ->setUsec($record["usec"]);
+
                  $this->getEm()->persist($event);
                  /*Ideda objekta*/
                  $this->getEm()->flush();
@@ -80,8 +122,7 @@ class APIDataService extends ContainerAware {
         $fromID= $data['records'][$array_lenght-1]['id'];
         //echo new id
         echo('Last inserted id from API: '.$fromID);
-    }
-    }
+    }}
     else
     {
     echo("Invalid table id ".$taleID);
