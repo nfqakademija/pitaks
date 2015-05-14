3ac5263ee07b1157cbed1486b00175192ed45ec8
Aurimas Valancius <aurimas.valancius@gmail.com>
2015-05-14 15:49:15 +0300

    added team view and delete alerts

--- src/pitaks/UserBundle/Resources/public/js/AlertModal.js
+++ src/pitaks/UserBundle/Resources/public/js/AlertModal.js
@@ -3,7 +3,7 @@
  */
 function modalAlert(text,redirect) {
     $('body').prepend(
-        ' <div class="myModal modal fade bs-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">'+
+        '<div class="myModal modal fade bs-example-modal-sm " tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">'+
         '<div class="modal-dialog modal-sm  " >'+
         '<div class=" modal-content "  style="padding: 5%; font-size: 20pt">'
         +text+'<div style="text-align: right">'+
